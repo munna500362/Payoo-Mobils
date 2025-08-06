8350f535f12a61a59338d9152376e021154a54d0
mehedi hasan <mehedihasan500362@gmail.com>
2025-08-06 22:36:31 +0600

    alert section done

--- js/inbox.js
+++ js/inbox.js
@@ -2,9 +2,18 @@
 
 document.getElementById('button-login').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('This is Login Button');
+    // console.log('This is Login Button');
 
-    const number = document.getElementById('data').value;
-    console.log(number);
+    const AccountNumber = document.getElementById('Input-mobile-number').value;
+    console.log(AccountNumber);
 
+    const AccountPin = document.getElementById('Input-pin-number').value;
+    console.log(AccountPin);
+
+    if(AccountNumber =='12345' && AccountPin=='123'){
+        console.log("Tik ace");
+    }
+    else{
+        alert("Kiccu tik nai")
+    }
 })
