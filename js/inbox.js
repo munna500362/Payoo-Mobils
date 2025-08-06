ecb49d74411494aaa0ea78f136b4e8f0e366b9aa
mehedi hasan <mehedihasan500362@gmail.com>
2025-08-06 23:27:56 +0600

    add money and cash out button added

--- js/inbox.js
+++ js/inbox.js
@@ -5,13 +5,13 @@ document.getElementById('button-login').addEventListener('click', function(event
     // console.log('This is Login Button');
 
     const AccountNumber = document.getElementById('Input-mobile-number').value;
-    console.log(AccountNumber);
+    // console.log(AccountNumber);
 
     const AccountPin = document.getElementById('Input-pin-number').value;
-    console.log(AccountPin);
+    // console.log(AccountPin);
 
     if(AccountNumber =='12345' && AccountPin=='123'){
-        console.log("Tik ace");
+        window.location.href = "account.html";
     }
     else{
         alert("Kiccu tik nai")
