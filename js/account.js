26dcdf0f6386c6577f1540b6fa91b45cbe77b4f7
mehedi hasan <mehedihasan500362@gmail.com>
2025-08-10 23:36:55 +0600

    Ad Money Form Js file complite

---
+++ js/account.js
@@ -0,0 +1,28 @@
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const AdmoneyInput = document.getElementById('input-Admoney').value;
+         AdmoneyNumber = parseFloat(AdmoneyInput);
+        
+    const AdmoneyPassword = document.getElementById('Admoney-pass').value;
+        AdmoneyNumberPass= parseFloat(AdmoneyPassword);
+        console.log(typeof AdmoneyNumberPass);
+    
+
+    if(AdmoneyPassword === '12345'){
+        Admoney = document.getElementById('Amount-number').innerText;
+         CurentMoney = parseFloat(Admoney);
+        
+
+        NewBalance = CurentMoney + AdmoneyNumber;
+        
+       
+        console.log(NewBalance);
+
+    }
+
+    else{
+        alert("Wrong Password");
+    }
+    
+})
