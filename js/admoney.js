9247fa872e591b163fb05247df326e0bc24e9e48
mehedi hasan <mehedihasan500362@gmail.com>
2025-08-11 01:03:26 +0600

    It was final comment. It is complete now

--- js/admoney.js
+++ js/admoney.js
@@ -3,19 +3,21 @@ document.getElementById('btn-login').addEventListener('click', function(event){
 
     const AdmoneyInput = document.getElementById('input-Admoney').value;
          AdmoneyNumber = parseFloat(AdmoneyInput);
+         console.log(AdmoneyNumber);
         
     const AdmoneyPassword = document.getElementById('Admoney-pass').value;
         AdmoneyNumberPass= parseFloat(AdmoneyPassword);
+        console.log(AdmoneyPassword);
         // console.log(typeof AdmoneyNumberPass);
     
 
-    if(AdmoneyPassword === '12345'){
-        Admoney = document.getElementById('Amount-number').innerText;
+    if(AdmoneyPassword === '123'){
+        let Admoney = document.getElementById('Amount-number').innerText;
          CurentMoney = parseFloat(Admoney);
         
 
         NewBalance = CurentMoney + AdmoneyNumber;
-        
+        console.log(NewBalance);
         document.getElementById('Amount-number').innerText = NewBalance;
 
     }
